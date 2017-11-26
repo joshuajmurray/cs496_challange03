aacef1b10e5070ee9af340e6b634a356f6007142
joshua murray <joshuajmurray@gmail.com>
2017-11-25 16:40:50 -0800

    call fragments from main activity, need to implement name build and reset still

--- app/src/main/java/com/jmurray/android/challenge03/MainActivity.java
+++ app/src/main/java/com/jmurray/android/challenge03/MainActivity.java
@@ -1,30 +1,46 @@
 package com.jmurray.android.challenge03;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     private Button mNiceButton;
     private Button mRudeButton;
     private EditText mFirstName;
-    private Names names = new Names();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNiceButton = (Button)findViewById(R.id.mangle_nice);
+        mFirstName = (EditText) findViewById(R.id.first_name);
+
+        mNiceButton = (Button) findViewById(R.id.mangle_nice);
         mNiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                call fragement here
-                mFirstName = (EditText)findViewById(R.id.first_name);
-                names.setLastName(true);
-                mFirstName.setText(names.getLastName());
+                if(mFirstName.getText().toString().matches("")) {
+                    Toast.makeText(MainActivity.this, R.string.no_first_name, Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    setContentView(R.layout.fragment_nice);
+                    FragmentManager manager = getSupportFragmentManager();
+                    Fragment fragment = manager.findFragmentById(R.id.nice_fragment);
+
+                    if (fragment == null) {
+                        fragment = createFragment();
+                        manager.beginTransaction()
+                                .add(R.id.nice_fragment, fragment)
+                                .commit();
+                    }
+                }
             }
         });
 
@@ -32,13 +48,27 @@ public class MainActivity extends AppCompatActivity {
         mRudeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                call fragment here
-                mFirstName = (EditText)findViewById(R.id.first_name);
-                names.setLastName(false);
-                mFirstName.setText(names.getLastName());
+                if(mFirstName.getText().toString().matches("")) {
+                    Toast.makeText(MainActivity.this, R.string.no_first_name, Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    setContentView(R.layout.fragment_rude);
+                    FragmentManager manager = getSupportFragmentManager();
+                    Fragment fragment = manager.findFragmentById(R.id.rude_fragment);
+
+                    if (fragment == null) {
+                        fragment = createFragment();
+                        manager.beginTransaction()
+                                .add(R.id.rude_fragment, fragment)
+                                .commit();
+                    }
+                }
             }
         });
 
     }
 
+    protected  Fragment createFragment() {
+        return new Fragment();
+    }
 }
--- app/src/main/java/com/jmurray/android/challenge03/RudeFragment.java
+++ app/src/main/java/com/jmurray/android/challenge03/RudeFragment.java
@@ -1,108 +1,29 @@
 package com.jmurray.android.challenge03;
 
-import android.content.Context;
-import android.net.Uri;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jmurray.android.challenge03.databinding.FragmentRudeBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RudeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RudeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RudeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public RudeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RudeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RudeFragment newInstance(String param1, String param2) {
-        RudeFragment fragment = new RudeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    private Names names = new Names();
+    public static RudeFragment newInstance() {
+        return new RudeFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rude, container, false);
-    }
+        FragmentRudeBinding binding = DataBindingUtil
+                .inflate(inflater, R.layout.fragment_rude, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        return binding.getRoot();
     }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+//                    mFirstName = (EditText) findViewById(R.id.first_name);
+//                    names.setLastName(false);
+//                    mFirstName.setText(names.getLastName());
 }
